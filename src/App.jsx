bf3f9c883c470895edeb5704a21358e772537c1d
samU13 <samu13lop@gmail.com>
2025-01-22 15:54:23 +0100

    frist git

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import About from './sections/About'
+import Hero from './sections/Hero'
+
+const App = () => {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden ">
+      <Hero />
+      <About />
+    </main>
+  )
+}
+
+export default App
